908bb331ef874070b50b7640bfb0131218a65105
HUANG, Zifeng <403918719@qq.com>
2020-03-30 05:07:23 -0700

    增加分配任务部分

--- Lab1/src/sudoku/main.cc
+++ Lab1/src/sudoku/main.cc
@@ -1,14 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-
-#include "showError.h" 	//print Error msg by errorCode
+#include <pthread.h>
+#include "showError.h" 	         //print Error msg by errorCode
 #include "sudoku.h"		//Need to write! includes Algorithms etc
+
+
+
+int nextpuzzleToBeDone=0;
+int puzzleNum = 0;  //数独总数量
+
+pthread_mutex_t puzzleQueueMutex=PTHREAD_MUTEX_INITIALIZER;
+
+int recvApuzzle()        //动态分配任务
+{
+
+  int currentpuzzleID=0;
+  pthread_mutex_lock(&puzzleQueueMutex);
+  if(nextpuzzleToBeDone>=puzzleNum) 
+  {
+    pthread_mutex_unlock(&puzzleQueueMutex);
+    return -1;
+  }
+
+  currentpuzzleID=nextpuzzleToBeDone;
+  nextpuzzleToBeDone++;
+  pthread_mutex_unlock(&puzzleQueueMutex);
+  return currentpuzzleID;
+}
+
+
 
 int main(int argc, char* argv[])
 {
 	//Counting
-	int puzzleNum = 0;
+	
 	int solutionNum = 0;
 	//Recording Puzzle
 	char puzzle[82];	
